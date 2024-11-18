e90f587a55db067647fd03bec34708292296e1a0
shijisan <cjsantos2411638@gmail.com>
2024-11-19 02:38:50 +0800

    front-end progress 2

--- app/product/[id]/page.jsx
+++ app/product/[id]/page.jsx
@@ -7,11 +7,28 @@ export default function ProductDetailsPage({ params }) {
 	const [product, setProduct] = useState(null);
 	const [error, setError] = useState("");
 	const router = useRouter();
+	const [quantity, setQuantity] = useState(1); 
+
+	const handleChange = (e) => {
+		const value = parseInt(e.target.value, 10);
+
+		if (!isNaN(value) && value > 0) {
+			setQuantity(value);
+		}
+	};
+
+	const addQuantity = () => {
+		setQuantity((prevQuantity) => prevQuantity + 1);
+	};
+
+	const subtractQuantity = () => {
+		setQuantity((prevQuantity) => (prevQuantity > 1 ? prevQuantity - 1 : 1)); 
+	};
 
 	useEffect(() => {
 		const fetchProduct = async () => {
 			try {
-				const { id } = await params; // Extract ID from params
+				const { id } = await params;
 
 				const res = await fetch(`/api/product/${id}`);
 				if (!res.ok) {
@@ -52,40 +69,57 @@ export default function ProductDetailsPage({ params }) {
 
 	return (
 		<>
-		<section className="flex items-center justify-center min-h-screen gap-4 pt-[10vh]">
-
-			<div className="flex w-full">
-				<div className="flex flex-col items-center justify-center w-1/2 h-[90vh] p-10">
-					<div className="flex items-center justify-center object-cover w-full border h-4/6 aspect-square">
-						<img
-							src={product.imageUrl}
-							alt={product.name}
-							className="object-cover"
-						/>
+			<section className="flex items-center justify-center min-h-screen gap-4 pt-[10vh]">
+				<div className="flex w-full">
+					<div className="flex flex-col items-center justify-center w-1/2 h-[90vh] p-10">
+						<div className="flex items-center justify-center border aspect-square">
+							<img
+								src={product.imageUrl}
+								alt={product.name}
+								height={300}
+								width={300}
+								className="object-cover"
+							/>
+						</div>
 					</div>
-					<div className="flex w-full h-2/6">
-						<img src="https://placehold.co/500/webp" className="w-1/3 border" />
-						<img src="https://placehold.co/500/webp" className="w-1/3 border" />
-						<img src="https://placehold.co/500/webp" className="w-1/3 border" />
+					<div className="flex items-center w-1/2 p-5">
+						<div className="flex flex-col justify-center p-5 space-y-4 bg-white border shadow rounded-xl">
+							<h1 className="text-2xl font-semibold">{product.name}</h1>
+							<p className="text-lg font-semibold">
+								Price: <span className="font-normal">&#8369;</span>
+								{product.price.toFixed(2)}
+							</p>
+							<p className="text-lg">{product.description}</p>
+							<div className="flex">
+								<button
+									className="px-3 border border-black"
+									onClick={subtractQuantity}
+								>
+									-
+								</button>
+								<input
+									className="w-2/5 p-2 text-center border border-black rounded-none border-x-0 focus:outline-none"
+									onChange={handleChange}
+									value={quantity}
+									type="number"
+									min={1}
+									id="quantity"
+								/>
+								<button className="px-3 border border-black" onClick={addQuantity}>
+									+
+								</button>
+							</div>
+							<button className="w-1/2 p-2 transition-colors bg-blue-100 rounded hover:bg-blue-200 text-blue-950">
+								Add to Cart
+							</button>
+							<button className="w-1/2 p-2 transition-colors bg-pink-200 rounded hover:bg-pink-300 text-blue-950">
+								Buy Now
+							</button>
+						</div>
+
 					</div>
 				</div>
-				<div className="flex flex-col justify-center w-1/2 p-10 space-y-4">
-					<h1 className="text-2xl font-semibold">{product.name}</h1>
-					<p className="text-lg font-semibold">Price: <span className="font-normal">&#8369;</span>{product.price.toFixed(2)}</p>
-					<p className="text-lg">{product.description}</p>
-					<div className="flex">
-						<button className="px-3 border border-black">-</button>
-						<input className="w-2/5 p-2 border border-black rounded-none border-x-0 focus:outline-none" min={1} type="number"/>
-						<button className="px-3 border border-black">+</button>
-					</div>
-					<button className="w-1/2 p-2 border border-black rounded ">Add to Cart</button>
-					<button className="w-1/2 p-2 border border-black rounded">Buy Now</button>
-
-				</div>
-			</div>
-
-		</section>
+			</section>
 		</>
-
 	);
 }
